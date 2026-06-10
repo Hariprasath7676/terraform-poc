798c40081d0d548b696254b976817e3e06fea40d
Hariprasath7676 <hariprasath7676@gmail.com>
2026-06-10 11:34:36 +0000

    Updated variable definitions for Gaia

--- variables.tf
+++ variables.tf
@@ -1,5 +1,14 @@
-variable "aws_region" {}
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
 
-variable "vpc_name" {}
+variable "vpc_name" {
+  description = "VPC Name"
+  type        = string
+}
 
-variable "vpc_cidr" {}
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+}
